11e607c0095b79e7040c34a2cb0dc7fe0976da9b
Artyom Vecherov <you@example.com>
2019-03-21 04:13:09 +0300

    cleaned from comments and testing code

--- src/queue.js
+++ src/queue.js
@@ -35,26 +35,4 @@ class PriorityQueue {
 	}
 }
 
-
-let q = new PriorityQueue();
-const nodes = [
-	{ priority: 10, data: 1 },
-	{ priority: 20, data: 2 },
-	{ priority: 5, data: 3 },
-	{ priority: 0, data: 4 },
-	{ priority: 8, data: 5 },
-	{ priority: 12, data: 6 },
-	{ priority: 17, data: 7 },
-	{ priority: 15, data: 8 },
-];
-
-const expectedData = [2, 7, 8, 6, 1, 5, 3, 4]
-
-nodes.forEach(node => q.push(node.data, node.priority));
-for (var i = 0; i < expectedData.length; i++) {
-	console.log(q.heap);
-	console.log(expectedData[i]);
-	console.log(q.shift());
-}
-
 module.exports = PriorityQueue;
